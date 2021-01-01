893c19f8e70e5345d4fc68ed6812ee7b47518999
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2021-01-01 14:30:39 -0500

    added automatic scroll:

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,6 +15,29 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 ////Options
 const { username, Room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
+const autoscroll = () => {
+    //new message element
+    const $newMessage = $messages.lastElementChild;
+    //height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+    //visible height
+    const visibleHeight = $messages.offsetHeight
+
+    //height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    //how far have i scrolled
+
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+
+}
 
 socket.on("message", (welcomeData) => {
     console.log(welcomeData)
